75eaa06c4efd79d8d57c67bb746629c28bdc09a6
Mora Unie Youer <mora_unie_youer@riseup.net>
2022-03-12 20:07:51 +0000

    Created "Hello, world" elisp file

---
+++ reset-mode.el
@@ -0,0 +1,40 @@
+;;; reset-mode.el --- Major mode for editing Reset language source code -*- lexical-binding: t -*-
+;;
+;; Filename: reset-mode.el
+;; Description: Major mode for editing Reset language source code
+;; Author: Mora Unie Youer <mora_unie_youer@riseup.net>
+;; Maintainer: Mora Unie Youer <mora_unie_youer@riseup.net>
+;; Copyright (c) 2022 Mora Unie Youer
+;; Created: Mar 12 2022
+;; URL: https://github.com/mora-unie-youer/reset-mode
+;;      https://gitlab.com/mora-unie-youer/reset-mode
+;;      https://notabug.org/mora-unie-youer/reset-mode
+;;      https://codeberg.org/mora-unie-youer/reset-mode
+
+;;; Commentary:
+;;
+;; Major mode for editing Reset language source code.
+
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included in all
+;; copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
+;;; Code:
+
+(print "Hello, world!")
+
+;;; reset-mode.el ends here
